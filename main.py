ab03f817dadbbc32b677587d8eb0daf03d2960bf
Eyedfish025 <jonatasgustavo24@gmail.com>
2026-05-16 18:03:01 -0300

    Refactor login template and enhance checkbox functionality

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask
 from models import *
-from route import *
+
+load_dotenv()
 
 app = Flask(__name__)
+app.secret_key = os.environ.get('FLASK_SECRET_KEY')
+
+from views import *
 
 Base.metadata.create_all(bind=dbaula)
 
